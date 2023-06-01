9f6d998b21a030f72adae8f71d53bd4f910908e7
Wenqi Glantz <wenqiglantz@gmail.com>
2023-06-01 08:35:13 -0400

    Update kb.py per latest llama_index release

--- DevSecOpsKB-observability/kb.py
+++ DevSecOpsKB-observability/kb.py
@@ -1,4 +1,4 @@
-from llama_index import SimpleDirectoryReader, LLMPredictor, PromptHelper, StorageContext, ServiceContext, GPTVectorStoreIndex, load_index_from_storage
+from llama_index import SimpleDirectoryReader, LLMPredictor, StorageContext, ServiceContext, GPTVectorStoreIndex, load_index_from_storage
 from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 import gradio as gr
@@ -7,25 +7,24 @@ import graphsignal
 
 load_dotenv()
 
-graphsignal.configure(api_key=os.getenv('graphsignal_api_key'), deployment='DevSecOpsKB')
+graphsignal.configure(api_key=os.getenv('GRAPHSIGNAL_API_KEY'), deployment='DevSecOpsKB')
 
-def create_service_context():
 
-    #constraint parameters
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
+# set context window
+context_window = 4096
+# set number of output tokens
+num_output = 512
 
-    #allows the user to explicitly set certain constraint parameters
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+#LLMPredictor is a wrapper class around LangChain's LLMChain that allows easy integration into LlamaIndex
+llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_output))
 
-    #LLMPredictor is a wrapper class around LangChain's LLMChain that allows easy integration into LlamaIndex
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.5, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
+#constructs service_context
+service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, context_window=context_window, num_output=num_output)
 
-    #constructs service_context
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-    return service_context
+#set the global service context object
+from llama_index import set_global_service_context
+set_global_service_context(service_context)
+
 
 
 def data_ingestion_indexing(directory_path):
@@ -34,9 +33,7 @@ def data_ingestion_indexing(directory_path):
     documents = SimpleDirectoryReader(directory_path).load_data()
     
     #when first building the index
-    index = GPTVectorStoreIndex.from_documents(
-        documents, service_context=create_service_context()
-    )
+    index = GPTVectorStoreIndex.from_documents(documents)
 
     #persist index to disk, default "storage" folder
     index.storage_context.persist()
@@ -49,7 +46,7 @@ def data_querying(input_text):
     storage_context = StorageContext.from_defaults(persist_dir="./storage")
 
     #loads index from storage
-    index = load_index_from_storage(storage_context, service_context=create_service_context())
+    index = load_index_from_storage(storage_context)
     
     #queries the index with the input text
     response = index.as_query_engine().query(input_text)
